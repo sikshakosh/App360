3271522d81cfb609ea9e5643fd2efe57866e544b
WC <wideclassroom@gmail.com>
2021-09-03 10:18:18 +0530

    nav graph added

--- app/src/main/java/com/android/app360/ui/welcome/WelcomeActivity.java
+++ app/src/main/java/com/android/app360/ui/welcome/WelcomeActivity.java
@@ -5,17 +5,23 @@ import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -41,11 +47,22 @@ import java.util.ArrayList;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+   private WelcomeViewModel welcomeViewModel;
+   private NavController navCtrl;
+
+   private Toolbar toolbar=null;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        welcomeViewModel = new ViewModelProvider(this).get(WelcomeViewModel.class);
+        NavHostFragment host = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_container);
+        navCtrl = host.getNavController();
+        AppBarConfiguration appBarConfiguration =
+                new AppBarConfiguration.Builder(navCtrl.getGraph()).build();
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 //        welcomeViewModel = new ViewModelProvider(this).get(WelcomeViewModel.class);
 //
 //
@@ -65,35 +82,39 @@ public class WelcomeActivity extends AppCompatActivity {
 
     }
 
+   public WelcomeViewModel getViewModel() {
+        return welcomeViewModel;
+   }
 
 
 
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.welcome_menu,menu);
+        return true;
+    }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        binding.toolbar.inflateMenu(R.menu.welcome_menu);
-//        return super.onCreateOptionsMenu(menu);
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-//        Intent intent = null;
-//        switch (item.getItemId()){
-//            case R.id.signUp:
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Intent intent = null;
+        switch (item.getItemId()){
+            case R.id.signUp:
+                navCtrl.navigate(R.id.action_welcomeFragment_to_moreListFragment);
 //                intent = new Intent(this, SignupActivity.class);
 //                this.startActivity(intent);
-//                return true;
-//
-//            case R.id.login:
-//                intent = new Intent(this,LoginActivity.class);
-//                this.startActivity(intent);
-//                return true;
-//
-//            default:
-//                return super.onOptionsItemSelected(item);
-//        }
-//        //return super.onOptionsItemSelected(item);
-//    }
+                return true;
+
+            case R.id.login:
+                intent = new Intent(this,LoginActivity.class);
+                this.startActivity(intent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        //return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/com/android/app360/ui/welcome/WelcomeFragment.java
+++ app/src/main/java/com/android/app360/ui/welcome/WelcomeFragment.java
@@ -1,9 +1,12 @@
 package com.android.app360.ui.welcome;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.core.content.ContextCompat;
@@ -15,11 +18,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import com.android.app360.R;
 import com.android.app360.databinding.ActivityWelcomeBinding;
 import com.android.app360.databinding.FragmentWelcomeBinding;
+import com.android.app360.ui.login.LoginActivity;
+import com.android.app360.ui.signup.SignupActivity;
 import com.android.app360.ui.welcome.viewmodel.WelcomeViewModel;
 import com.android.appcompose.composable.utility.cardgrid.CardGridRecyclerViewAdapter;
 import com.android.appcompose.composable.utility.cardgrid.model.ParentModel;
@@ -74,22 +81,26 @@ public class WelcomeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        welcomeViewModel = new ViewModelProvider(getActivity()).get(WelcomeViewModel.class);
-        parentModelArrayList.add(new ParentModel(DataType.FEATURED_CLASSROOMS));
-        parentModelArrayList.add(new ParentModel(DataType.FEATURED_MENTORS));
+
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        DISPLAYED_CLASSROOM_COUNT = 0;
+        DISPLAYED_MENTOR_COUNT = 0;
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_welcome,container,false);
-
+        welcomeViewModel = ((WelcomeActivity)getActivity()).getViewModel();
+        parentModelArrayList.add(new ParentModel(DataType.FEATURED_CLASSROOMS));
+        parentModelArrayList.add(new ParentModel(DataType.FEATURED_MENTORS));
         binding.setWelcomeViewModel(welcomeViewModel);
         binding.setLifecycleOwner(getActivity());
         imageSliderView = binding.bannerList;
         initDotIndicator(Color.TRANSPARENT);
         layoutSubviews();
-        setupRecyclerView();
+
 
 
 
@@ -155,76 +166,20 @@ public class WelcomeFragment extends Fragment {
             }
 
 
-        });parentModelArrayList.add(new ParentModel(DataType.FEATURED_CLASSROOMS));
-        parentModelArrayList.add(new ParentModel(DataType.FEATURED_MENTORS));
-
-        welcomeViewModel.getLocalClassrooms().observe(getActivity(), classrooms -> {
-            if(classrooms.isEmpty()){
-                AsyncTask.execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        welcomeViewModel.getRemoteClassrooms();
-
-                    }
-                });
-            }
-            if(DISPLAYED_CLASSROOM_COUNT<GRID_ITEM_COUNT){
-                ParentModel classroomParent = (ParentModel) parentModelArrayList.get(0);
-
-                int counter = 0;
-
-                for(ClassroomModel cm: classrooms){
-                    if(DISPLAYED_CLASSROOM_COUNT<GRID_ITEM_COUNT){
-                        classroomParent.getData().add(classrooms.get(counter));
-                        DISPLAYED_CLASSROOM_COUNT +=1;
-                        counter+=1;
-                    }else{
-                        break;
-                    }
-
-                }
-                setupRecyclerView();
-            }
-
-
-
-
         });
 
-        welcomeViewModel.getLocalMentors().observe(getActivity(), mentors -> {
-            if(mentors.isEmpty()){
-                AsyncTask.execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        welcomeViewModel.getRemoteMentors();
-
-                    }
-                });
-            }
-
-            if(DISPLAYED_MENTOR_COUNT<GRID_ITEM_COUNT){
-                ParentModel classroomParent = (ParentModel) parentModelArrayList.get(1);
-
-                int counter = 0;
-                for(MentorModel cm: mentors){
-                    if(DISPLAYED_MENTOR_COUNT<GRID_ITEM_COUNT){
-                        classroomParent.getData().add(mentors.get(counter));
-                        DISPLAYED_MENTOR_COUNT+=1;
-                        counter +=1;
-                    }else{
-                        break;
-                    }
-
-                }
-                setupRecyclerView();
-            }
 
 
-        });
 
         return binding.getRoot();
         // Inflate the layout for this fragment
         //return inflater.inflate(R.layout.fragment_welcome, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setupRecyclerView();
     }
 
     void initDotIndicator(int bgColor){
@@ -267,4 +222,8 @@ public class WelcomeFragment extends Fragment {
             parentAdapter.notifyDataSetChanged();
         }
     }
+
+
+
+
 }
